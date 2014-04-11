cb38ee48d1af0e4ff26936ff91b5738a2d452fa8
abr4xas <me@abr4xas.org>
2014-04-11 14:34:15 -0430

    typo

--- paquetes.sh
+++ paquetes.sh
@@ -10,7 +10,7 @@ if [[ $USER != root ]]; then
    exit 1
 fi
 apt-get install -f
-apt-get install -y git flashplugin-nonfree firmware-linux firmware-linux-free firmware-linux-nonfree firmware-realtek linux-wlan-ng -t experimental iceweasel
+apt-get install -y -t experimental iceweasel git flashplugin-nonfree firmware-linux firmware-linux-free firmware-linux-nonfree firmware-realtek linux-wlan-ng
 apt-get autoremove -y
 
 echo "Cual es el RELEASE de Brackets(ir a http://brackets.io/)"
