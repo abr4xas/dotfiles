0d0aa9efaa5dc39148ed3638401d36b81e512df0
abr4xas <me@abr4xas.org>
2014-12-09 08:59:38 -0500

    modificado

--- nginx.sh
+++ nginx.sh
@@ -16,20 +16,5 @@ echo "############################"
 apt-get install nginx -y
 service nginx start
 update-rc.d nginx defaults
-echo " "
-echo " "
-echo " Ahora vamos a configurar los Virtual Hosts "
-echo " "
-echo " "
-
-echo " Ingrese el nombre del dominio a crear. Ej: dominio.com"
-read MY_VH
-mkdir -p /var/www/$MY_VH/public_html
-chown -R www-data:www-data /var/www/$MY_VH/public_html
-chmod 755 /var/www
-echo "Success: You Have Set Up a Virtual Host" >> /var/www/example.com/$MY_VH/index.html
-cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$MY_VH
-
-
 
 exit 0
