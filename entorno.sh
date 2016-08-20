7f53e4e7d54bf1ae194f6eda79ab8e3df5061e4e
abr4xas <me@abr4xas.org>
2016-08-20 19:05:52 -0400

    Actualizado Agregando varias modificaciones y eliminando cosas :smile:

--- entorno.sh
+++ entorno.sh
@@ -14,24 +14,22 @@ fi
 if [ -e ~/.gitconfig ]; then
     rm ~/.gitconfig
     ln -s $PWD/gitconfig ~/.gitconfig
-    else
-    ln -s $PWD/gitconfig ~/.gitconfig
 fi
-#if [ -e ~/.irssi/config ]; then
-#	rm ~/.irssi/config
-#	ln -s $PWD/config ~/.irssi/config
-#fi
+if [ -e ~/.zshrc ]; then
+    rm ~/.zshrc
+    ln -s $PWD/zshrc ~/.zshrc
+fi
 if [ -e ~/.gitignore ]; then
     rm ~/.gitignore
     ln -s $PWD/gitignore ~/.gitignore
-    else
-    ln -s $PWD/gitignore ~/.gitignore
 fi
-if [ -e ~/.gitmessage.txt ]; then
+if [ -e ~/gitmessage.txt ]; then
     rm ~/gitmessage.txt
     ln -s $PWD/gitmessage.txt ~/.gitmessage.txt
-    else
-    ln -s $PWD/gitmessage.txt ~/.gitmessage.txt
+fi
+if [ -e ~/.oh-my-zsh/themes/honukai.zsh-theme ]; then
+    rm ~/.oh-my-zsh/themes/honukai.zsh-theme
+    ln -s $PWD/honukai.zsh-theme ~/.oh-my-zsh/themes/honukai.zsh-theme
 fi
 echo "Finalizado"
 exit 0;
