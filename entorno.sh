b6d4b5aac85996d6cab72878a83610627b4e91b0
abr4xas <me@abr4xas.org>
2016-09-04 18:45:44 -0400

    Actualizado mejoras en el archivo entorno. :smile:

--- entorno.sh
+++ entorno.sh
@@ -11,25 +11,37 @@ if [[ $USER = root ]]; then
     exit 1
 fi
 
+wget -c https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm-zsh/master/honukai.zsh-theme > /dev/null 2>&1
+
 if [ -e ~/.gitconfig ]; then
     rm ~/.gitconfig
     ln -s $PWD/gitconfig ~/.gitconfig
+else
+   ln -s $PWD/gitconfig ~/.gitconfig
 fi
 if [ -e ~/.zshrc ]; then
     rm ~/.zshrc
+    ln -s $PWD/zshrc ~/.zshrc
+else
     ln -s $PWD/zshrc ~/.zshrc
 fi
 if [ -e ~/.gitignore ]; then
     rm ~/.gitignore
     ln -s $PWD/gitignore ~/.gitignore
+else
+    ln -s $PWD/gitignore ~/.gitignore
 fi
-if [ -e ~/gitmessage.txt ]; then
-    rm ~/gitmessage.txt
+if [ -e ~/.gitmessage.txt ]; then
+    rm ~/.gitmessage.txt
+    ln -s $PWD/gitmessage.txt ~/.gitmessage.txt
+else
     ln -s $PWD/gitmessage.txt ~/.gitmessage.txt
 fi
 if [ -e ~/.oh-my-zsh/themes/honukai.zsh-theme ]; then
     rm ~/.oh-my-zsh/themes/honukai.zsh-theme
-    ln -s $PWD/honukai.zsh-theme ~/.oh-my-zsh/themes/honukai.zsh-theme
+    cp $PWD/honukai.zsh-theme ~/.oh-my-zsh/themes/honukai.zsh-theme
+else
+    cp $PWD/honukai.zsh-theme ~/.oh-my-zsh/themes/honukai.zsh-theme
 fi
 echo "Finalizado"
 exit 0;
